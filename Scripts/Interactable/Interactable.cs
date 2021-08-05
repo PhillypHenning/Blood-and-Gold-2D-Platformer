3eda465fa21f32d650c42f33266f727cd0b0f27a
Phillyp Henning <phillyp.henning@gmail.com>
2021-08-05 11:36:53 -0400

    Preparring Interactable base script

---
+++ Scripts/Interactable/Interactable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Review the Collectable for an idea of how to build this
+    // This will be a base script for Interactables
+    // The differences between an interactable and a collectable are;
+    //      1. Interactables lock normal character motion while
+    //      2. Are interacted with manually instead of automatically (requires a button press)
+
+    
+}
