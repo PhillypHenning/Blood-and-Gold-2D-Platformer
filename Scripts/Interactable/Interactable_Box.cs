799f655f06de2dcdf1d744393379f9d3cc988fca
Phillyp Henning <phillyp.henning@gmail.com>
2021-08-08 16:51:05 -0400

    Adding Interactable_Box script

---
+++ Scripts/Interactable/Interactable_Box.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Box : Interactable
+{
+    // TODO: Enable Reward when health reaches 0
+}
