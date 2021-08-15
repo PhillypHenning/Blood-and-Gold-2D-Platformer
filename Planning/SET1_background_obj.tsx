d57a156cf0c0b1f083fa7136d9403bbf0eb0fe44
Phillyp Henning <phillyp.henning@gmail.com>
2021-08-15 18:03:59 -0400

    oFfIcIaL dOcUmEnTaTiOn - Weston Vincze 2021

---
+++ Planning/SET1_background_obj.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="SET1_background_obj" tilewidth="32" tileheight="32" tilecount="924" columns="28">
+ <image source="SET1_background_obj.png" width="912" height="1072"/>
+</tileset>
