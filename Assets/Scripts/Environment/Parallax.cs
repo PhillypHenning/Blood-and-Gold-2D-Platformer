3c94e2d268a0276cb29275f2814dd4f13140689d
Phillyp Henning <phillyp.henning@gmail.com>
2021-08-18 22:56:45 -0400

    Distance from x added to Parallax

--- Assets/Scripts/Environment/Parallax.cs
+++ Assets/Scripts/Environment/Parallax.cs
@@ -8,17 +8,34 @@ public class Parallax : MonoBehaviour
     private SpriteRenderer _SpriteRenderer;
     private float _ZPosStart;
 
-    public Camera _Camera;
-    public Transform _Subject;
+    private GameObject _Subject;
+    private Camera _Camera;
+    private Transform _SubjectTransform;
 
-    public Vector2 Travel => (Vector2)_Camera.transform.position - _StartPosition;
-    public float _DistanceFromSubject => transform.position.z - _Subject.position.z; 
-    public float _ClippingPlane => (_Camera.transform.position.z + (_DistanceFromSubject > 0 ? _Camera.farClipPlane : _Camera.nearClipPlane));
+    private Vector2 Travel => (Vector2)_Camera.transform.position - _StartPosition;
+    private float _DistanceFromSubject => transform.position.z - _SubjectTransform.position.z; 
+    private float _ClippingPlane => (_Camera.transform.position.z + (_DistanceFromSubject > 0 ? _Camera.farClipPlane : _Camera.nearClipPlane));
 
-    public float _ParallaxFactor => Mathf.Abs(_DistanceFromSubject / _ClippingPlane);
+    private float _ParallaxFactor => Mathf.Abs(_DistanceFromSubject / _ClippingPlane);
+
+    private Sprite _Sprite;
+    private Rect _Rect;
+    private float _MiddlePos;
+
+    private void Awake() {
+        _Subject = GameObject.Find("Player");
+        _Camera = _Subject.GetComponentInChildren<Camera>();
+        _SubjectTransform = _Subject.transform;
+    }
 
     private void Start() {     
         _SpriteRenderer = GetComponent<SpriteRenderer>();
+        _Sprite = _SpriteRenderer.sprite;
+        _Rect = _Sprite.rect;
+        _MiddlePos = _Rect.width / 2;
+
+        Debug.Log(_Sprite.bounds.size.x);
+        Debug.Log(_Sprite.bounds.size.y);
 
         _StartPosition = transform.position;
         _ZPosStart = transform.position.z;
@@ -32,9 +49,33 @@ public class Parallax : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 newPos = _StartPosition + Travel * _ParallaxFactor;
-        //newPos.y = newPos.y * 2;
-        transform.position = new Vector3(newPos.x, newPos.y, _ZPosStart);
-        //Loop();
+        StartParallaxEffect();
+        DetermineBackgroundLoadDirection();
     }
+
+    private void StartParallaxEffect(){
+        Vector2 newPos = _StartPosition + Travel * _ParallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, _ZPosStart);
+    }
+
+    private void DetermineBackgroundLoadDirection(){
+        // Determine where the subject is relative to the position between either side and the half way point
+        //float cur = 
+        //Debug.Log(_Rect);
+        Vector3 distsub1 = new Vector3(_StartPosition.x, 0, 0);
+        Vector3 distsub2 = new Vector3(_Subject.transform.position.x, 0, 0);
+        
+        float Xdistance = Vector3.Distance(distsub1, distsub2);
+        //Debug.Log(_ParallaxFactor);
+        //Debug.Log("Redrawn when " +  (1000 * _ParallaxFactor / 3));
+        //Debug.Log(Xdistance);
+        float redrawAt = (1000 * _ParallaxFactor / 2.75f);
+        if(Xdistance > redrawAt){
+            Debug.Log("Redrawing layer: " + this.name);
+            // TODO: Enable the second set
+        }
+
+    }
+
+
 }
--- Assets/Scripts/Environment/ParallaxManager.cs
+++ Assets/Scripts/Environment/ParallaxManager.cs
@@ -35,27 +35,131 @@ public class ParallaxManager : MonoBehaviour
     /*
         Needs to know 
     */
-    [SerializeField] private SpriteRenderer _BackgroundSpriteRenderer;
-    private Sprite _BackgroundSprite; 
-    
 
-    [SerializeField] private float _BackgroundDepthZ = 35f; // 35 default
+    public Camera _Camera;
+    public Transform _Subject;
 
+
+    // BACKGROUND \\
+    private GameObject _Background1GameObject;
+    private SpriteRenderer _Background1SpriteRenderer;
+    private Sprite _Background1Sprite; 
+    private Rect _Background1Rect;
+    private Vector2 _Background1StartPos;
+    public Vector2 _BackgroundTravel;
+
+    private float _Background1HorizontalSize;
+    private float _Background1VerticalSize;
+    private float _Background1ZLayer = 35f;
+
+    // MiddleBackground \\
+    private GameObject _MiddleBackground1GameObject;
+    private SpriteRenderer _MiddleBackground1SpriteRenderer;
+    private Sprite _MiddleBackground1Sprite; 
+    private Rect _MiddleBackground1Rect;
+
+    private float _MiddleBackground1HorizontalSize;
+    private float _MiddleBackground1VerticalSize;
+    private float _MiddleBackground1ZLayer = 21f;
+
+    // MiddleForeground \\
+    private GameObject _MiddleForeground1GameObject;
+    private SpriteRenderer _MiddleForeground1SpriteRenderer;
+    private Sprite _MiddleForeground1Sprite; 
+    private Rect _MiddleForeground1Rect;
+
+    private float _MiddleForeground1HorizontalSize;
+    private float _MiddleForeground1VerticalSize;
+    private float _MiddleForeground1ZLayer = 12;
+
+    // Foreground \\
+    private GameObject _Foreground1GameObject;
+    private SpriteRenderer _Foreground1SpriteRenderer;
+    private Sprite _Foreground1Sprite; 
+    private Rect _Foreground1Rect;
+
+    private float _Foreground1HorizontalSize;
+    private float _Foreground1VerticalSize;
+    private float _Foreground1ZLayer = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        _BackgroundSprite = _BackgroundSpriteRenderer.GetComponent<Sprite>();
-        Debug.Log(_BackgroundSprite);
+        // BACKGROUND \\
+        _Background1GameObject = GameObject.Find("Background_P_1");
+        _Background1SpriteRenderer = _Background1GameObject.GetComponent<SpriteRenderer>();
+        _Background1Sprite = _Background1SpriteRenderer.sprite;
+        
+        _Background1Rect = _Background1Sprite.rect;
+        _Background1HorizontalSize = _Background1Rect.width; 
+        _Background1VerticalSize = _Background1Rect.height;
+
+
+        // MIDDLEBACKGROUND \\
+        _MiddleBackground1GameObject = GameObject.Find("MiddleBackground_P_1");
+        _MiddleBackground1SpriteRenderer = _MiddleBackground1GameObject.GetComponent<SpriteRenderer>();
+        _MiddleBackground1Sprite = _MiddleBackground1SpriteRenderer.sprite;
+        
+        _MiddleBackground1Rect = _MiddleBackground1Sprite.rect;
+        _MiddleBackground1HorizontalSize = _MiddleBackground1Rect.width; 
+        _MiddleBackground1VerticalSize = _MiddleBackground1Rect.height;
+
+        // MiddleForeground \\
+        _MiddleForeground1GameObject = GameObject.Find("MiddleForeground_P_1");
+        _MiddleForeground1SpriteRenderer = _MiddleForeground1GameObject.GetComponent<SpriteRenderer>();
+        _MiddleForeground1Sprite = _MiddleForeground1SpriteRenderer.sprite;
+        
+        _MiddleForeground1Rect = _MiddleForeground1Sprite.rect;
+        _MiddleForeground1HorizontalSize = _MiddleForeground1Rect.width; 
+        _MiddleForeground1VerticalSize = _MiddleForeground1Rect.height;
+
+        // Foreground \\
+        _Foreground1GameObject = GameObject.Find("Foreground_P_1");
+        _Foreground1SpriteRenderer = _Foreground1GameObject.GetComponent<SpriteRenderer>();
+        _Foreground1Sprite = _Foreground1SpriteRenderer.sprite;
+        
+        _Foreground1Rect = _Foreground1Sprite.rect;
+        _Foreground1HorizontalSize = _Foreground1Rect.width; 
+        _Foreground1VerticalSize = _Foreground1Rect.height;
+        
+        SetBackgroundLayer();
+        SetMiddleBackgroundLayer();
+        SetMiddleForegroundLayer();
+        SetForegroundLayer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DetermineBackgroundLoadDirection();
     }
 
-    private void SetBackground(){
+    private void SetBackgroundLayer(float newX=0, float newY=0, float newZ=0){
+        _Background1GameObject.transform.position = new Vector3(newX, newY, _Background1ZLayer);
+    }
 
+    private void SetMiddleBackgroundLayer(float newX=0, float newY=0, float newZ=0){
+        _MiddleBackground1GameObject.transform.position = new Vector3(newX, newY, _MiddleBackground1ZLayer);
+    }
+
+    private void SetMiddleForegroundLayer(float newX=0, float newY=0, float newZ=0){
+        _MiddleForeground1GameObject.transform.position = new Vector3(newX, newY, _MiddleForeground1ZLayer);
+    }
+
+    private void SetForegroundLayer(float newX=0, float newY=0, float newZ=0){
+        _Foreground1GameObject.transform.position = new Vector3(newX, newY, _Foreground1ZLayer);
+    }
+
+
+    private void DetermineBackgroundLoadDirection(){
+        // Find where the player is
+
+        // Based on the middle point of the current 
+    }
+
+
+
+    public float MiddleOfBackground(){
+        return _Background1HorizontalSize / 2;
     }
 }
