01a3fb23c51477c0c94e3186a426505c88a99aeb
Phillyp Henning <phillyp.henning@gmail.com>
2021-09-10 23:40:41 -0400

    Small boi implemented

---
+++ Assets/Scripts/AI/Decisions/DecideToJump.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Decisions/Decide to Jump", fileName = "DecideToJump")]
+public class DecideToJump : AIDecision
+{
+    private Collider2D _TargetCollider;
+
+    public float _DetectArea = 3f;
+    public LayerMask _TargetMask;
+
+
+    public override bool Decide(StateController controller)
+    {
+        return DecidingToJump(controller);
+    }
+
+    private bool DecidingToJump(StateController controller)
+    {
+        _TargetCollider = Physics2D.OverlapCircle(controller.transform.position, _DetectArea, _TargetMask);
+        if (_TargetCollider != null && controller._CharacterJump.CharacterCanJump)
+        {   
+            Debug.Log("Jumping");
+            controller._Target = _TargetCollider.transform;
+            controller._CharacterJump.TriggerJump();
+            return true;
+        }
+        return false;
+    }
+}
