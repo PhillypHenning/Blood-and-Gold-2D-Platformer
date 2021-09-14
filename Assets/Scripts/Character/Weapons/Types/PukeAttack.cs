c4dd4990124aec85bf9d7a87049360197b762641
Phillyp Henning <phillyp.henning@gmail.com>
2021-09-13 20:22:36 -0400

    Fixed boss attack

---
+++ Assets/Scripts/Character/Weapons/Types/PukeAttack.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PukeAttack : Weapon
+{
+    protected override void PlayShootingSFX()
+    {
+    }
+
+    protected override void PlayReloadSFX()
+    {
+        
+    }
+}
